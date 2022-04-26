0361f64732f2a3667cf5ba7b3e47b2578d0fe05a
diegomoli <diegomoli92@gmail.com>
2022-04-26 00:42:52 -0300

    Control props alt

--- src/hooks/useShoppingCart.ts
+++ src/hooks/useShoppingCart.ts
@@ -14,31 +14,15 @@ export const useShoppingCart = () => {
         product: Product;
     }) => {
         setShoppingCart((oldShoppingCart) => {
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || {
-                ...product,
-                count: 0,
-            };
-
-            //Si todo esto es mayor a cero es porque lo puedo incrementar
-            if (Math.max(productInCart.count + count, 0) > 0) {
-                productInCart.count += count;
-                return {
-                    ...oldShoppingCart,
-                    [product.id]: productInCart,
-                };
+         
+            if (count === 0) {
+              const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+              return rest;
             }
-            //Si el producto no existe o la sumatoria es negativo se elimina el carrito
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            return rest;
-
-            // if (count === 0) {
-            //   const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            //   return rest;
-            // }
-            // return {
-            //   ...oldShoppingCart,
-            //   [product.id]: { ...product, count },
-            // };
+            return {
+              ...oldShoppingCart,
+              [product.id]: { ...product, count },
+            };
         });
     }
       return{shoppingCart,onProductCountChange}
